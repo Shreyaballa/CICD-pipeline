116701d3429cf1cd995f3626273c8c68fda36230
root <root@ip-172-31-3-116.us-east-2.compute.internal>
2025-06-23 11:59:03 +0000

    Add PHP contact page

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-Hello from Jenkins CI/CD
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact Info</title>
+</head>
+<body>
+    <h2>Contact Information</h2>
+    <p><strong>Name:</strong> Shreyasri Balla</p>
+    <p><strong>Email:</strong> shreyasriballa@gmail.com</p>
+    <p><strong>Phone:</strong> +91-9876543210</p>
+</body>
+</html>
+
